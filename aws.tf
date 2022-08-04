9217f677647f1651e99009aa388e0de9988b489a
anandbritto <anandbritto@gmail.com>
2022-08-04 11:15:06 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {       # Resource Name
     ami = "ami-076e3a557efe1aa9c"    # amazon ami 
     instance_type = "t2.micro"
     count = "1"
-    key_name = "Anand-key"
+    key_name = "Anand_Key"
     subnet_id = "subnet-0d47c7cc17ec8f644"
 
     tags = {
